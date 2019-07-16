65cc2346c6cd02415fee4820fd2dfac3200b9517
Matthew Reynolds <rey.mtr@gmail.com>
2019-07-15 22:14:37 -0400

    Update example main.c to test semaphores

--- src/main.c
+++ src/main.c
@@ -6,12 +6,25 @@
 
 #include "../rtos/rtos.h"
 
+rtosSemaphore_t semaphore;
+
 void task1(void* arg) {
   uint32_t task_id = (uint32_t) arg;
 
   while (true) {
-    rtosDelay(task_id * 100);
+    rtosDelay(5);
+    rtosSemaphoreAcquire(&semaphore, rtosWaitForever);
   }
+}
+
+void task2(void* arg) {
+  uint32_t task_id = (uint32_t) arg;
+
+  while (true) {
+    rtosDelay(100);
+    rtosSemaphoreRelease(&semaphore);
+  }
+
 }
 
 int main(void) {
@@ -19,6 +32,9 @@ int main(void) {
   rtosTaskHandle_t tcb1;
   rtosTaskHandle_t tcb2;
   rtosTaskNew(task1, (void*) 1, RTOS_PRIORITY_NORMAL, &tcb1);
-  rtosTaskNew(task1, (void*) 2, RTOS_PRIORITY_NORMAL, &tcb2);
+  rtosTaskNew(task2, (void*) 2, RTOS_PRIORITY_NORMAL, &tcb2);
+
+  rtosSemaphoreNew(0, 0, NULL, &semaphore);
+
   rtosBegin();
 }
