ac3d69dfc340732d9acd29b56fb7eae6d8694bc0
Matthew Reynolds <rey.mtr@gmail.com>
2019-07-24 21:04:37 -0400

    Add comments to semaphore barrier test

--- test/test_semaphore_blocking.c
+++ test/test_semaphore_blocking.c
@@ -1,88 +1,101 @@
 /**
- * test_mutex_prioinherit.c
+ * test_semaphore_blocking.c
  *
- * Test mutex priority inheritance
+ * Test blocking semaphores using a barrier
  */
 #if TEST_SEMAPHORE
- 
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "../rtos/rtos.h"
 
-rtosMutex_t printMutex;
+typedef struct {
+  rtosMutex_t     mutex;
+  rtosSemaphore_t turnstile1, turnstile2;
+  uint32_t        count, n;
+} barrier_t;
+
+void init(barrier_t* b, uint32_t n) {
+  rtosMutexNew(NULL, &b->mutex);
+  rtosSemaphoreNew(n, 0, NULL, &b->turnstile1);
+  rtosSemaphoreNew(n, 1, NULL, &b->turnstile2);
+  b->count = 0;
+  b->n     = n;
+}
+
+barrier_t        b;
+rtosMutex_t      print_mutex;
 rtosTaskHandle_t task1;
 rtosTaskHandle_t task2;
 rtosTaskHandle_t task3;
 
-typedef struct {
-	rtosMutex_t mutex;
-	rtosSemaphore_t turnstile1, turnstile2;
-	uint32_t count, n;
-} barrier_t;
+void sync(barrier_t* b) {
 
-void init(barrier_t *b, uint32_t n) {
-	//osMutexAttr_t attr = { .attr_bits = osMutexPrioInherit | osMutexRobust };
-	//b->mutex = osMutexNew(&attr);
-	rtosMutexNew(NULL, &b->mutex);
-  rtosSemaphoreNew(n, 0, NULL, &b->turnstile1);
-  rtosSemaphoreNew(n, 1, NULL, &b->turnstile2);
-	b->count = 0;
-	b->n = n;
+  // Increment the count
+  rtosMutexAcquire(&b->mutex, RTOS_WAIT_FOREVER);
+  b->count++;
+  if (b->count == b->n) {
+    rtosSemaphoreAcquire(&b->turnstile2, RTOS_WAIT_FOREVER);
+    rtosSemaphoreRelease(&b->turnstile1);
+  }
+  rtosMutexRelease(&b->mutex);
+
+  // Wait on turnstile 1
+  rtosSemaphoreAcquire(&b->turnstile1, RTOS_WAIT_FOREVER);
+  rtosSemaphoreRelease(&b->turnstile1);
+
+  // Decrement the count
+  rtosMutexAcquire(&b->mutex, RTOS_WAIT_FOREVER);
+  b->count--;
+  if (b->count == 0) {
+    rtosSemaphoreAcquire(&b->turnstile1, RTOS_WAIT_FOREVER);
+    rtosSemaphoreRelease(&b->turnstile2);
+
+    rtosMutexAcquire(&print_mutex, RTOS_WAIT_FOREVER);
+    printf("Tasks have reached the barrier\n");
+    rtosMutexRelease(&print_mutex);
+  }
+  rtosMutexRelease(&b->mutex);
+
+  // Wait on turnstile 2
+  rtosSemaphoreAcquire(&b->turnstile2, RTOS_WAIT_FOREVER);
+  rtosSemaphoreRelease(&b->turnstile2);
 }
 
-void sync(barrier_t * b) {
-	rtosMutexAcquire(&printMutex, RTOS_WAIT_FOREVER);
-	printf("SYNC \n");
-	rtosMutexRelease(&printMutex);
-	rtosMutexAcquire(&b->mutex, RTOS_WAIT_FOREVER); {
-		b->count++;
-		if(b->count == b->n) {
-			rtosSemaphoreAcquire(&b->turnstile2, RTOS_WAIT_FOREVER);
-			rtosSemaphoreRelease(&b->turnstile1);
-		}
-	} rtosMutexRelease(&b->mutex);
+void task(void* args) {
+  uint32_t task_id = (uint32_t) args;
 
-	rtosSemaphoreAcquire(&b->turnstile1, RTOS_WAIT_FOREVER);
-	rtosSemaphoreRelease(&b->turnstile1);
+  while (true) {
+    rtosDelay(rtosGetSysTickFreq() / (rand() % 3 + 1));
 
-	rtosMutexAcquire(&b->mutex, RTOS_WAIT_FOREVER); {
-		b->count--;
-		if(b->count == 0) {
-			rtosSemaphoreAcquire(&b->turnstile1, RTOS_WAIT_FOREVER);
-			rtosSemaphoreRelease(&b->turnstile2);
+    rtosMutexAcquire(&print_mutex, RTOS_WAIT_FOREVER);
+    printf("Task %d: Syncing...\n", task_id);
+    rtosMutexRelease(&print_mutex);
 
-			rtosMutexAcquire(&printMutex, RTOS_WAIT_FOREVER);
-			printf("Tasks have synced\n");
-			rtosMutexRelease(&printMutex);
-		}
-	} rtosMutexRelease(&b->mutex);
-	rtosSemaphoreAcquire(&b->turnstile2, RTOS_WAIT_FOREVER);
-	rtosSemaphoreRelease(&b->turnstile2);
-}
+    sync(&b);
 
-barrier_t b;
-void task(void *args) {
-	while(true) {
-		rtosDelay(rtosGetSysTickFreq()/(rand()%3 + 1));
-		sync(&b);
-		rtosMutexAcquire(&printMutex, RTOS_WAIT_FOREVER);
-		printf("Finished Sync \n");
-		rtosMutexRelease(&printMutex);
-	}
+    rtosMutexAcquire(&print_mutex, RTOS_WAIT_FOREVER);
+    printf("Task %d: Finished Sync\n", task_id);
+    rtosMutexRelease(&print_mutex);
+  }
 }
 
 int main(void) {
-	srand(100);
-	rtosInitialize();
-	rtosMutexNew(NULL, &printMutex);
-	init(&b, 3);
-	rtosTaskNew(task, NULL, RTOS_PRIORITY_NORMAL, &task1);
-	rtosTaskNew(task, NULL, RTOS_PRIORITY_NORMAL, &task2);
-	rtosTaskNew(task, NULL, RTOS_PRIORITY_NORMAL, &task3);
-	rtosBegin();
-	for( ; ; ) ;
+  printf("\n\n\n\n\n");
+
+  srand(100);
+  rtosInitialize();
+
+  init(&b, 3);
+
+  rtosTaskNew(task, (void*) 1, RTOS_PRIORITY_NORMAL, &task1);
+  rtosTaskNew(task, (void*) 2, RTOS_PRIORITY_NORMAL, &task2);
+  rtosTaskNew(task, (void*) 3, RTOS_PRIORITY_NORMAL, &task3);
+  rtosMutexNew(NULL, &print_mutex);
+
+  rtosBegin();
 }
 
 #endif
